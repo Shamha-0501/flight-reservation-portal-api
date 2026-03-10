592b4aaa97ce18a37b04c7dc0362cf266ed33312
mrfarvees-official <saroathfarvees@gmail.com>
2026-03-11 03:14:14 +0530

    Bugfix on random session

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -6,7 +6,6 @@ use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 use Illuminate\Routing\Middleware\SubstituteBindings;
-use Illuminate\Session\Middleware\StartSession;
 use Illuminate\View\Middleware\ShareErrorsFromSession;
 use Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful;
 
@@ -22,7 +21,6 @@ return Application::configure(basePath: dirname(__DIR__))
             EnsureFrontendRequestsAreStateful::class,
             EncryptCookies::class,
             AddQueuedCookiesToResponse::class,
-            StartSession::class,
             ShareErrorsFromSession::class,
             SubstituteBindings::class
         ]);
